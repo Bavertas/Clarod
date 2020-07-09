b9bd268d84bcfeff5a608f30dba78fb0fa915813
Bavertas <baverta@gmail.com>
2020-07-08 19:24:47 -0700

    son odevler eklendi

---
+++ (if-Statements) Assignment - 1.py
@@ -0,0 +1,10 @@
+
+name = input("Waht is your name?")
+
+if (name == "Joseph") :
+
+    print("Hello, Joseph! The password is : W@12") 
+
+else :
+
+    print("Hello, Amina! See you later.")
---
+++ Covid 19 Assigment.py
@@ -0,0 +1,37 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+age = input("Are you a cigarette addict older than 75 years old?")
+
+chornic = input("Do you have a severe chronic disease? ")
+
+immune = input("Is your immune system too weak?")
+
+if (age == "yes" and chornic == "yes" and immune == "yes") :
+
+    print("You are in risky group")
+
+elif (age == "no" or chornic == "no" or immune == "no") :
+
+     print('You are not in risky group')
+
+else :
+
+    print('Please try again')
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
